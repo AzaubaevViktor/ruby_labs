fd4517dd18f8dda1a780d84c658a6e1696608100
Korovin Viktor <sarvash.inc@gmail.com>
2020-01-28 12:20:42 +0700

    Second task :: select is good

--- 2.rb
+++ 2.rb
@@ -1,3 +1,6 @@
+require 'sourcify'
+
+
 class BigArray
   attr_accessor :size
   attr_accessor :content
@@ -15,36 +18,168 @@ class BigArray
   end
 
   def map(&block)
-    @content.each_slice(@thread_count).map { |slice|
-      Thread.new {
-        Thread.current[:results] = slice.map { |item| block.call item}
-      }
-    }.map do |t|
+    split_content(block).map do |t|
       t.join
       t[:results]
     end.flatten
   end
 
+  def any?(&block)
+    threads = split_content(block)
+    
+    threads.cycle {
+        |thread|
+      print thread, ': ', thread.status, "\n"
+      if thread.status != 'run'
+        thread.join
+        print "#{thread[:results]}", "\n"
+        # this for any, for all the same
+        if thread[:results].any?
+          print "WE FOUND IT, KILL ALL\n"
+          threads.map {
+              |th|
+            print "Kill #{th}\n"
+            th.kill
+          }
+          threads.clear
+          return true
+        else
+          threads.delete(thread)
+        end
+      end
+    }
+    
+    false
+  end
+
+  def all?(&block)
+    threads = split_content(block)
+
+    threads.cycle {
+        |thread|
+      print thread, ': ', thread.status, "\n"
+      if thread.status != 'run'
+        thread.join
+        print "#{thread[:results]}", "\n"
+        # this for any, for all the same
+        if !thread[:results].all?
+          print "WE FOUND IT, KILL ALL\n"
+          threads.map {
+              |th|
+            print "Kill #{th}\n"
+            th.kill
+          }
+          threads.clear
+          return false
+        else
+          threads.delete(thread)
+        end
+      end
+    }
+
+    true
+  end
+
+  def select(&block)
+    split_content(block).map do |t|
+      t.join
+      items = Hash[t[:values].zip t[:results]]
+      t[:values].select { |item| items[item] }
+    end.flatten
+  end
+
   def to_s
-    "BigArray aka " + @content.to_s
+    'BigArray aka ' + @content.to_s
+  end
+
+  private
+
+  def split_content(block)
+    @content.each_slice(@thread_count).map { |slice|
+      Thread.new {
+        Thread.current[:values] = slice
+        Thread.current[:results] = slice.map { |item| block.call item }
+      }
+    }
   end
 end
 
 
+class Array
+  def test_it_man(range, &block)
+    results = []
+
+    puts "...::::````` Start testing for  `````::::..."
+    puts ": BLOCK:"
+    puts ": #{block.to_source(:strip_enclosure => true)}"
+
+    each { |cl|
+      puts ".~~~~ test for: #{cl}  ~~~~"
+      puts ': ready...'
+      a = cl.new
+      puts ': steady...'
+
+      range.each { |x| a.push x }
+
+      puts ": #{a}"
+
+      puts ': GO!'
+      start = Time.now
+      results.push(block.call(a))
+      tm = Time.now - start
+      puts ": Finish by #{tm}"
+    }
+
+    puts "... Check results"
+
+    raise "Size is 0" unless results.size != 0
+    raise "Calculation error: #{results}" unless results.all? { |item| item == results[0] }
+
+    puts "`````::::... NNICE ...::::`````"
+
+  end
+end
+
+
+[BigArray, Array].test_it_man((1..27)) { |a|
+  a.select {|x| sleep x / 15; x > 5}
+}
+
+
 [BigArray, Array].each { |cl|
-  p " ~~~~ MAP for: " + cl.to_s + " ~~~~"
-  p "ready..."
+  p ' ~~~~ ANY? for: ' + cl.to_s + ' ~~~~'
+  p 'ready...'
   a = cl.new
-  p "steady..."
+  p 'steady...'
 
   (1..27).each {|x| a.push x}
 
   puts a
 
-  p "GO!"
+  p 'GO!'
   start = Time.now
-  a.map {|x| sleep x / 10; x ** x ** 5}
+  p a.any? {|x| sleep x / 15; x % 19 == 0}
   tm = Time.now - start
-  p "Finish"
+  p 'Finish'
   p tm
 }
+
+
+[BigArray, Array].each { |cl|
+  p ' ~~~~ MAP for: ' + cl.to_s + ' ~~~~'
+  p 'ready...'
+  a = cl.new
+  p 'steady...'
+
+  (1..27).each {|x| a.push x}
+
+  puts a
+
+  p 'GO!'
+  start = Time.now
+  a.map {|x| sleep x / 15; x ** x ** 5}
+  tm = Time.now - start
+  p 'Finish'
+  p tm
+}
+
